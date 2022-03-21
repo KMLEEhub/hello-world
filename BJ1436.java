28912e1094d92b34af31e93103883cbe317892b9
KMLEEhub <102021326+KMLEEhub@users.noreply.github.com>
2022-03-21 21:52:14 +0900

    Add files via upload

---
+++ BJ1436.java
@@ -0,0 +1,32 @@
+package baekjooon;
+
+import java.util.Scanner;
+
+//영화감독 숌
+public class BJ1436 {
+    public static void main(String[] args) {
+        int cnt = 0;
+        int answer = 666;
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        while (true) {
+            // answer이 666연달아 포함한 수이면 cnt++
+            boolean flag = false;
+            String a = Integer.toString(answer);
+            for (int i = 0; i < a.length() - 2; i++) {
+                if (a.charAt(i) == '6' && a.charAt(i + 1) == '6' && a.charAt(i + 2) == '6') {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)
+                cnt++;
+            // cnt==x 이면 answer출력 break;
+            if (cnt == x) {
+                System.out.println(answer);
+                break;
+            }
+            answer++;
+        }
+    }
+}
